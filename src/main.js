b3aa9338a286868c6d14aabfa7dee05b1db570ca
Thomas Nichay <thomasnichay@yahoo.com.br>
2019-08-13 11:55:43 -0300

    modulo 2

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import {Usuario,Admin,map,filter,find,uniao,usuario3} from './modulo1'
+//modulo 1
+import {Usuario as Usuario_m1,Admin,map,filter,find,uniao,usuario3} from './modulo1'
 
-const User1 = new Usuario('email@teste.com', 'senha123');
+const User1 = new Usuario_m1('email@teste.com', 'senha123');
 const Adm1 = new Admin('email@teste.com', 'senha123');
 console.log(User1.isAdmin()); // false
 console.log(Adm1.isAdmin()); // true
@@ -10,3 +11,15 @@ console.log(find);
 console.log(uniao);
 console.log(usuario3);
 
+//modulo 2
+//1.1
+import {Usuario as ClasseUsuario} from './functions(modulo2)';
+
+ClasseUsuario.info();
+
+//1.2
+import {idade} from './functions(modulo2)';
+console.log(idade);
+
+//1.3
+import {Usuario, idade as IdadeUsuario} from './functions(modulo2)';
